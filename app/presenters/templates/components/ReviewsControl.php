9c3f9c88a55835a21d24083e359edbbc9147e32b
Jan Martinek <info@jan-martinek.com>
2015-10-13 07:28:59 +0200

    Bugfix: missing template filters in reviews' list

--- app/presenters/templates/components/ReviewsControl.php
+++ app/presenters/templates/components/ReviewsControl.php
@@ -6,9 +6,22 @@ use Nette\Application\UI\Control;
 
 class ReviewsControl extends Control
 {
+	public function setTemplateFilters($template) 
+	{
+    	//markdown
+        $template->addFilter('md', function ($s) {
+            return \Michelf\Markdown::defaultTransform($s);
+        });
+        //inline markdown
+        $template->addFilter('imd', function ($s) {
+            return strip_tags(Markdown::defaultTransform($s), '<a><strong><em>');
+        });
+    }	
+	
     public function render(Array $reviews)
     {
         $template = $this->template;
+        $this->setTemplateFilters($template);
         $template->setFile(__DIR__ . '/reviews.latte');
         $template->reviews = $reviews;
         $template->render();
